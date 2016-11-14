18feb661de2b8842a9a166ec1217dc39828319ef
pawellasota <plasota76@gmail.com>
2016-11-14 14:40:25 +0100

    workableversion

--- adv.py
+++ adv.py
@@ -19,7 +19,7 @@ def ret_matrix(x_coord, y_coord, x=30, y=30):
                 matrix[row].append('X')
             else:
                 matrix[row].append('.')
-        matrix[y_coord][x_coord] = "@"
+    matrix[y_coord][x_coord] = "@"
     return matrix
 
 #def put_obstacles(x,y,z,c): #block
@@ -35,18 +35,19 @@ def print_matrix(lista):
 
 x_coord, y_coord = 10, 10
 #print_matrix(ret_matrix(x_coord, y_coord))
+#list_matrix = []
 
 while True:
     list_matrix=ret_matrix(x_coord, y_coord, 30, 30)
     print_matrix(list_matrix)
     char = getch()
-    if (char == "a") and (matrix[y_coord][x_coord-1] != "X"):
+    if (char == "a") and (list_matrix[y_coord][x_coord-1] != "X"):
         x_coord -= 1
-    elif (char == "d") and (matrix[y_coord][x_coord+1] != "X"):
+    elif (char == "d") and (list_matrix[y_coord][x_coord+1] != "X"):
         x_coord += 1
-    elif (char == "w") and (matrix[y_coord-1][x_coord] != "X"):
+    elif (char == "w") and (list_matrix[y_coord-1][x_coord] != "X"):
         y_coord -= 1
-    elif char == "s" and (matrix[y_coord+1][x_coord-1] != "X"):
+    elif char == "s" and (list_matrix[y_coord+1][x_coord] != "X"):
         y_coord += 1
-    else:
+    elif (char == "q"):
         break
